21670d406695a017ce6694d7b5a1980d0bfe55d3
thetoothless <ninaling@ucla.edu>
2017-03-10 18:57:18 -0800

    test commit 5

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var http = require('http')
-var fs = require('fs')
-var stream = require('youtube-audio-stream')
+var http = require('http');
+var fs = require('fs');
+var stream = require('youtube-audio-stream');
 var express = require('express');
 var path = require('path');
 
@@ -9,19 +9,21 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get(/^\/watch/, function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/synaesthetic.html'));
   console.log("hai");
 });
 
 app.get(/youtube/, function (req, res) {
   stream(req.url.slice(1)).pipe(res);
   console.log("hai2");
-})
+});
 
 app.get('/', function(req, res) {
   res.render('index');
   console.log('hai3');
-})
+});
+
+console.log(process.env.PORT);
 
 app.set('port', process.env.PORT || 3000);
 
